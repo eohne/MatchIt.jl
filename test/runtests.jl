f794ad34ab9cc3166723055b43468819cbd6e518
eohne <elias.ohneberg@gmail.com>
2023-07-13 12:08:31 +0100

    Fix Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,9 @@ using DataFrames, GLM, PlotlyJS
 using Test
 
 data = DataFrame(y = Int.(round.(rand(100),digits=0)), x1 = rand(100),x2=rand(100),id=repeat(collect(1:10),10))
-@testset "PSM" begin
-    ta = PSM(data,"y",["x1","x2"],["id"],LogitLink(),Inf);
-    @test typeof(ta) <: PSMatched
+@testset "psmatch" begin
+    ta = psmatch(data,"y",["x1","x2"],["id"],LogitLink(),Inf);
+    @test typeof(ta) <: PSM
     @test nrow(ta.df) == nrow(data)
     @test nrow(ta.matched) >0
     ta2 = match_table(ta);
