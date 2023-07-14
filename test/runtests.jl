7eacdc9efc8b2f2b565f028865e52995bc5dbc46
eohne <elias.ohneberg@gmail.com>
2023-07-14 21:48:19 +0100

    Fixed Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,9 @@ using DataFrames, GLM, PlotlyJS
 using Test
 
 data = DataFrame(y = Int.(round.(rand(100),digits=0)), x1 = rand(100),x2=rand(100),id=repeat(collect(1:10),10))
-@testset "psmatch" begin
-    ta = psmatch(data,"y",["x1","x2"],["id"],LogitLink(),Inf);
-    @test typeof(ta) <: PSM
+@testset "matchit" begin
+    ta = matchit(data,@formula(y + x1 + x2),exact = ["id"]);
+    @test typeof(ta) <: MatchedIt
     @test nrow(ta.df) == nrow(data)
     @test nrow(ta.matched) >0
     ta2 = match_table(ta);
