1ed01e4e68d669b8d3b2a98e2bf58ec5ccb446cc
eohne <elias.ohneberg@gmail.com>
2023-07-14 21:55:03 +0100

    Fix Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
-using PSMatching
+using MatchIt
 using DataFrames, GLM, PlotlyJS
 using Test
 
 data = DataFrame(y = Int.(round.(rand(100),digits=0)), x1 = rand(100),x2=rand(100),id=repeat(collect(1:10),10))
 @testset "matchit" begin
-    ta = matchit(data,@formula(y + x1 + x2),exact = ["id"]);
+    ta = matchit(data,@formula(y ~ x1 + x2),exact = ["id"]);
     @test typeof(ta) <: MatchedIt
     @test nrow(ta.df) == nrow(data)
     @test nrow(ta.matched) >0
