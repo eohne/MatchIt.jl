17d3030190ad1412768dfbae11ea9b1c1fff268f
eohne <elias.ohneberg@gmail.com>
2023-07-19 12:05:54 +0100

    Fix documentation of get_propensities

--- src/Propensities.jl
+++ src/Propensities.jl
@@ -1,18 +1,16 @@
 
 """
-    get_propensities(data::DataFrame,T::String,X::Vector{String},link::GLM.Link01)
+    get_propensities(data::DataFrame,f::FormulaTerm,link::GLM.Link01)
 
 Calculates the Propensity Scores and returns a `DataFrame` with a new column `:Dist`.
 
 # Arguments:
   * data`::DataFrame`: The DataFrame containg the data. This should have no missing values. Rows with missing values will be deleted.
-  * T`::String`: the name of the treatment variable
-  * X`::Vector{String}`: the names of the covariates to be matched on
+  * f`::FormulaTerm`: A formula specifying the functional form of the regression (use `@formual` to create it)
   * link`::GLM.Link01`: The link function used. One of `LogitLink()` or `ProbitLink()`
 
 # Output
   * `DataFrame` containing the original data without missing values that has a new column (`:Dist`) with the propensity scores. 
-
 """
 function get_propensities(data::DataFrame,f::FormulaTerm,link::GLM.Link01)
     out = copy(data)
@@ -32,18 +30,17 @@ function get_propensities(data::DataFrame,f::FormulaTerm,link::GLM.Link01)
 end
 
 """
-    get_propensities!(data::DataFrame,T::String,X::Vector{String},link::GLM.Link01)
+    get_propensities!(data::DataFrame,f::FormulaTerm,link::GLM.Link01)
 
-Calculates the Propensity Scores and returns a `DataFrame` with a new column `:PS`.
+Calculates the Propensity Scores and returns a `DataFrame` with a new column `:Dist`.
 
 # Arguments:
   * data`::DataFrame`: The DataFrame containg the data. This should have no missing values. Rows with missing values will be deleted.
-  * T`::String`: the name of the treatment variable
-  * X`::Vector{String}`: the names of the covariates to be matched on
+  * f`::FormulaTerm`: A formula specifying the functional form of the regression (use `@formual` to create it)
   * link`::GLM.Link01`: The link function used. One of `LogitLink()` or `ProbitLink()`
 
 # Output
-  * `DataFrame` containing the original data without missing values that has a new column (`:PS`) with the propensity scores. 
+  * `DataFrame` containing the original data without missing values that has a new column (`:Dist`) with the propensity scores. 
 
 """
 function get_propensities!(data::DataFrame,f::FormulaTerm,link::GLM.Link01)
